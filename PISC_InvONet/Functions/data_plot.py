da2344edfe7d5f91aa75b95235125d82d11fbe1c
qingqing5555 <72873901+qingqing5555@users.noreply.github.com>
2025-03-10 11:09:12 +0800

    Update data_plot.py

--- PISC_InvONet/Functions/data_plot.py
+++ PISC_InvONet/Functions/data_plot.py
@@ -29,7 +29,7 @@ def plotinitmodel(init, true, vmin, vmax, SaveFigPath):
 
 def plotinitsource(init, gt, SaveFigPath):
     """
-        打印震源
+       Print focal point
     """
     t = 500
     figsize = (12, 6)
@@ -47,7 +47,7 @@ def plotinitsource(init, gt, SaveFigPath):
 
 def plotsourcespectra(init_source, true_source, SaveFigPath):
     """
-        震源频谱图
+      Seismic source spectrum
     """
     init_source = init_source.astype(np.complex64).reshape(-1)
     true_source = true_source.astype(np.complex64).reshape(-1)
@@ -106,7 +106,7 @@ def plotoneshot(receiver_amplitudes_true, SaveFigPath):
 
 def plotcomparison(gt, pre, ite, SaveFigPath):
     """
-    打印真实模型和反演模型
+ Print the true model and inversion model
     """
 
     dim = gt.shape
